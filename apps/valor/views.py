cf32bb740fc6c1de9cba063224093e910426dc13
Isaac Fierro <isaaczid@hotmail.com>
2022-06-15 20:07:22 -0700

    feat(valor/views): Respuesta personalizada agregada

--- apps/valor/views.py
+++ apps/valor/views.py
@@ -1,5 +1,8 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
 from .serializers import ValorSerializer
+from apps.sensor.models import SensorModel
 
 
 class ValorViewSet(viewsets.ModelViewSet):
@@ -9,4 +12,13 @@ class ValorViewSet(viewsets.ModelViewSet):
         if pk is None:
             return self.serializer_class.Meta.model.objects.all()
 
-        return self.serializer_class.Meta.model.objects.filter(id=pk).first()
+        return self.serializer_class.Meta.model.objects.filter(valor_id=pk).first()
+    
+    def retrieve(self, request, pk):
+        valores_sensor = SensorModel.objects.filter(sensor_id=pk).first()
+        valores_filtrados = list(valores_sensor.valormodel_set.all().values('valor').values_list('valor', flat=True))
+        
+        return Response({
+            'mensaje': 'Hola',
+            'data': valores_filtrados
+        }, status=status.HTTP_200_OK)
