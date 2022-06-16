2ce0a7f91b69c1293729e022743409d9bb5b321c
Isaac Fierro <isaaczid@hotmail.com>
2022-06-15 20:05:35 -0700

    feat(valor/serializers): Serializador de app 'sensor' agregado

--- apps/valor/serializers.py
+++ apps/valor/serializers.py
@@ -1,10 +1,11 @@
-from dataclasses import fields
 from rest_framework import serializers
+from apps.sensor.serializers import SensorSerializer
 from .models import ValorModel
 
 
 class ValorSerializer(serializers.ModelSerializer):
+    # sensor = SensorSerializer()
     class Meta:
         model = ValorModel
         fields = "__all__"
-        read_only_fields = ["id"]
+        read_only_fields = ["valor_id"]
