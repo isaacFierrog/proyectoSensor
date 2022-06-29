629396aef77bb2d8126ce7262d4701678bdf5ae7
Isaac Fierro <isaaczid@hotmail.com>
2022-06-28 19:59:28 -0700

    feat(modulo/models): Modelo modificado

--- apps/modulo/models.py
+++ apps/modulo/models.py
@@ -1,5 +1,18 @@
+from click import version_option
 from django.db import models
 
 
 class ModuloModel(models.Model):
-    modulo_id = models.AutoField(primary_key=True)
+    modulo_id = models.CharField(
+        max_length=4,                          
+        primary_key=True, 
+        blank=True
+    )
+    
+    class Meta:
+        verbose_name = 'modulo'
+        verbose_name_plural = 'modulos'
+        db_table = 'modulo'
+        
+    def __str__(self):
+        return f"<Modulo: {self.modulo_id}>"
