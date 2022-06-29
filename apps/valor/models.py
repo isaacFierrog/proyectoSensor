fa93ad2b5271791e7455628cab75753b1831d5d7
Isaac Fierro <isaaczid@hotmail.com>
2022-06-28 20:02:07 -0700

    feat(valor/models): Modelo modificado

--- apps/valor/models.py
+++ apps/valor/models.py
@@ -6,3 +6,11 @@ class ValorModel(models.Model):
     valor_id = models.AutoField(primary_key=True)
     valor = models.PositiveIntegerField(default=0)
     sensor = models.ForeignKey(SensorModel, on_delete=models.CASCADE)
+    
+    class Meta:
+        verbose_name = 'valor'
+        verbose_name_plural = 'valores'
+        db_table = 'valor'
+        
+    def __str__(self):
+        return f'<Valor: {self.valor}>'
