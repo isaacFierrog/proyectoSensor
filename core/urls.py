6d8252625aba6d1927d968e45ac8c979d315acf3
Isaac Fierro <isaaczid@hotmail.com>
2022-06-28 20:04:22 -0700

    feat(core/urls): Rutas agregadas

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from apps.home.views import home
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("apps.valor.routes")),
-    path("api/", include("apps.sensor.routes")),
+    path('', home, name='home'),
+    path('sensores/', include(('apps.sensor.urls', 'sensor'))),
+    path('modulos/', include(('apps.modulo.urls', 'modulo')))
 ]
