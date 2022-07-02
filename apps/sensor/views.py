682ba205c7346564f76a52c0d23c913b30855aec
Isaac Fierro <isaaczid@hotmail.com>
2022-07-02 01:26:59 -0700

    feat(sensor/views): Vistas agregadas

--- apps/sensor/views.py
+++ apps/sensor/views.py
@@ -1,6 +1,36 @@
-from django.shortcuts import render
-from .forms import SensorForm
+from django.shortcuts import render, redirect
+from .models import SensorModel
 
 
-def sensores(request):
-    return render(request, 'sensor/sensor.html')
+def listar_sensor(request):
+    sensores = SensorModel.objects.filter(estado=True)
+    return render(request, 'sensor/listar_sensor.html', {
+        'sensores': sensores
+    })
+
+
+def crear_sensor(request):
+    if request.method == 'POST':
+        if 'crear_sensor' in request.POST:
+            sensor_id:str = hex(SensorModel.objects.count() + 1).split('0x')[1]
+            SensorModel.objects.create(
+                sensor_id=sensor_id
+            )
+            return redirect('sensor:listar_sensor')
+    
+    return render(request, 'sensor/crear_sensor.html')
+
+
+def eliminar_sensor(request, id):
+    print(id)
+    sensor = SensorModel.objects.filter(sensor_id=id, estado=True).first()
+    print(sensor)
+    
+    if request.method == 'POST':
+        sensor.estado = False
+        sensor.save()
+        return redirect('sensor:listar_sensor')
+        
+    return render(request, 'sensor/eliminar_sensor.html', {
+        'sensor': sensor
+    })
