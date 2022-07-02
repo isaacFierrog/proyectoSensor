87158d12caee07ad9e1b8980ab39369cc7669827
Isaac Fierro <isaaczid@hotmail.com>
2022-07-02 01:25:52 -0700

    feat(sensor/urls): Urls agregadas

--- apps/sensor/urls.py
+++ apps/sensor/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import sensores
+from .views import listar_sensor, crear_sensor, eliminar_sensor
 
 
 urlpatterns = [
-    path('', sensores, name='sensores')
+    path('listar/', listar_sensor, name='listar_sensor'),
+    path('crear/', crear_sensor, name='crear_sensor'),
+    path('eliminar/<id>/', eliminar_sensor, name='eliminar_sensor')
 ]
